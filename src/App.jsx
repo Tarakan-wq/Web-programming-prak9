19f86d219a7eb70265b9c8a50fdf15a5fe8ace3c
Tarakan-wq <tarasstar123@gmail.com>
2025-11-24 10:53:24 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,8 @@ export default function App() {
   return (
     <main>
       <BlogCard
-        image="assets/blogcar.png"
-        avatar="assets/avatar.female.png"
+        image="/Web-programming-prak9/assets/blogcar.png"
+        avatar="/Web-programming-prak9/assets/avatar.female.png"
         category="HTML & CSS"
         title="Практична №3"
         description="Мета практичної роботи полягає в тому, щоб налаштувати середовище розробки VS Code, а також реалізувати картку блогу для вдосконалення фундаментальних навичок з HTML та CSS."
