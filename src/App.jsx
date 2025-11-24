07f02d7b61be5de7704d1a4faec037084e813b3d
Tarakan-wq <tarasstar123@gmail.com>
2025-11-24 11:10:53 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import BlogCard from "./components/BlogCard";
 import "./components/Card.css";
+import avatar from "/assets/avatar.female.png";
+import blogcar from "/assets/blogcar.png";
 
 export default function App() {
   return (
     <main>
       <BlogCard
-          image="/Web-programming-prak9/assets/blogcar.png"
-        avatar="/Web-programming-prak9/assets/avatar.female.png"
+        image={blogcar}
+        avatar={avatar}
         category="HTML & CSS"
         title="Практична №3"
         description="Мета практичної роботи полягає в тому, щоб налаштувати середовище розробки VS Code, а також реалізувати картку блогу для вдосконалення фундаментальних навичок з HTML та CSS."
